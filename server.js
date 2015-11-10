36c728eaf6f9e8e9326615510b0959d67a556588
Alberto Chvaicer <achvaicer@gmail.com>
2015-11-09 23:34:33 -0200

    fazendo request e retornando propriedades

--- server.js
+++ server.js
@@ -10,9 +10,38 @@ app.get('/Cotacao.aspx', function(req, res) {
         request.post('http://bmf.chromestudio.com.br/server/api/acoes.php',
                         { form: { e: 'pontos', codigo: req.query.codigoAcao } },
                         function(error, response, body) {
-                                console.log(response);
-                                console.log(body);
-                                res.end(body);
+				
+				var data = JSON.parse(body);
+				if (data.err != 0) 
+					console.log(data);
+				
+				var funcao = req.query.funcao || "preco";
+				var ret = '';
+				switch(req.query.funcao)
+				{
+					case "variacao":
+					case "oscilacao":
+						ret = data.desc.oscilacao;
+						break;
+					case "maxima":
+					case "maximo":
+						ret = data.desc.maxima;
+						break;
+					case "minima":
+					case "minimo":
+						ret = data.desc.minima;
+						break;
+					case "abertura":
+						ret = data.desc.abertura;
+						break;
+					case "nome":
+						ret = data.desc.nome;
+						break;
+
+					default:
+						ret = data.desc.ultima;
+				}
+                                res.end(ret);
                         });
 
 });
